2fa16bcfe93d4eecb971c31ba6742a84c3714b83
dhanushshetty1 <dhanushshetty106@gmail.com>
2025-07-21 17:44:09 +0530

    Add App.py and basic_cal.py, update .idea files

---
+++ App.py
@@ -0,0 +1,83 @@
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+#
+
+# char_name = "Shetty"
+# char_age = 35
+#
+# print("HI, i am " +char_name+" ," + "he is "+str(char_age))
+# print(f"HI, I am {char_name}, he is {char_age}")
+# print("HI, I am {}, he is {}".format(char_name, char_age))
+# char_name = "Dhanush"
+# print("HI, I am %s, he is %d" % (char_name, char_age))
+
+
+# print ("\'Dhanush\nShetty\'")
+# phrase = "Dhanush Shetty"
+# print(phrase.upper())
+# print(phrase.isupper())
+# print(phrase.upper().isupper())
+# print(len(phrase))
+# print(phrase[0])
+# print(phrase.index("Sh"))
+# print(phrase.replace("Shetty","Good"))
+
+# from math import *
+#
+# print(7.3+(2*5))
+# num = -6
+# print(abs(num))
+# num = 5
+# print(pow(num,2))
+# print(floor(8.99))
+# print(ceil(8.99))
+# print(sqrt(16))
+
+
+# name = input("Enter your name : ")
+# age = input("Enter your age : ")
+# print("hello "+name +"You are " + age)
+
+# color = input("Enter your favourite color: ")
+# plural_noun = input("Enter your plural noun: ")
+# celebrity = input("Enter your celebrity: ")
+#
+# print(f"Roses are {color}")
+# print(f"{plural_noun} are blue")
+# print("i love "+celebrity)
+
+
+# friends = ["Shetty","Dhanush","Vivek","Nishchal"]
+# print(friends)
+# print(friends[1])
+# print(friends[-1])
+# print(friends[1:2])
+# friends[0] ="yo yo"
+
+
+# lucky_number = [4,8,15,16,23,42]
+# friends = ["Shetty","Dhanush","Vivek","Nishchal"]
+# friends.insert(0,"bhumm")
+# friends.remove("bhumm")
+# friends.clear()
+# friends.pop()
+# friends.sort()
+# lucky_number.sort()
+# lucky_number.reverse()
+# print(lucky_number)
+# print(friends.index("Vivek"))
+# print(friends.count("Vivek"))
+#
+# friends.append("Jim")
+# friends.extend(lucky_number)
+# print(friends)
+#
+# friends2 = friends.copy()
+
+
+coordinates = (4,5)
+list_cooridinates = [(4,5),(4,6),(4,7),(4,8),(4,9)]
+#cant change tuble object
+print(coordinates[0])
